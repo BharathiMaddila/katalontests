b8723b8e1b287c36da16dd256cf47e2b5ccf3476
BharathiMaddila <48004408+BharathiMaddila@users.noreply.github.com>
2019-06-14 00:43:46 -0700

    Update Script1560498081186.groovy

--- Scripts/New Test Case (2)/Script1560498081186.groovy
+++ Scripts/New Test Case (2)/Script1560498081186.groovy
@@ -13,3 +13,8 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+
+uh gsai oihos
+ oihdsf
+ ihg
+\dsoj
